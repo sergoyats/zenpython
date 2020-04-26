b3e915b2135445963033b6889805ad12f23bd6e2
sergoyats <abiens-abi@outlook.com>
2020-04-26 08:19:46 +0300

    first send on dev

---
+++ viewfunc1.py
@@ -0,0 +1,26 @@
+import csv
+from flask import Flask
+from faker import Faker
+
+fake_ru = Faker('ru_Ru')
+
+
+@app.route('/content')
+def content():
+    with open('requirements.txt', 'r') as file:
+        print(file.read())
+
+
+@app.route('/users')
+def users():
+    for i in range(1, 11):
+        print(i, fake_ru.name(), fake_ru.ascii_free_email())
+
+
+@app.route('/csv_read')
+def csv_read():
+    with open('hw.csv') as f:
+        h_w = list(csv.reader(f))[1:]
+        ah = sum([float(i[1]) for i in h_w]) / len(h_w)
+        aw = sum([float(j[2]) for j in h_w]) / len(h_w)
+        return f'The average height and weight: {ah} inches and {aw} lb'
